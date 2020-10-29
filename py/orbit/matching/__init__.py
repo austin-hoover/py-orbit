ff39cac3bdf589b904ef5c068a74fddc534a2f99
AustinHoover1218 <ahoover7@vols.utk.edu>
2020-10-29 11:31:50 -0400

    Remove old module
    The matching routine for the Danilov distribution is now contained in py/orbit/envelope/Envelope.py.

--- py/orbit/matching/__init__.py
+++ py/orbit/matching/__init__.py
@@ -1,9 +1,5 @@
 from matching import Optics, EnvelopeSolver
 
-from env_matching import DanilovMatcher, KVMatcher
-
 __all__ = []
 __all__.append('Optics')
 __all__.append('EnvelopeSolver')
-__all__.append('DanilovMatcher')
-__all__.append('KVMatcher')
