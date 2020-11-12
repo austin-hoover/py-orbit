37752275d9f629d548e6810947bf5e27b8578376
austin-hoover <ahoover7@vols.utk.edu>
2020-11-12 10:42:00 -0500

    Add method to change the perveance of the envelope solver nodes

--- py/orbit/space_charge/envelope/__init__.py
+++ py/orbit/space_charge/envelope/__init__.py
@@ -1,6 +1,10 @@
 from orbit.space_charge.envelope.EnvSolverNode import EnvSolverNode
-from orbit.space_charge.envelope.EnvSolverLatticeModifications import setEnvSolverNodes
+from orbit.space_charge.envelope.EnvSolverLatticeModifications import (
+    set_env_solver_nodes,
+    set_perveance,
+)
 
 __all__ = []
 __all__.append("EnvSolverNode")
-__all__.append("setEnvSolverNodes")
+__all__.append("set_env_solver_nodes")
+__all__.append("set_perveance")
