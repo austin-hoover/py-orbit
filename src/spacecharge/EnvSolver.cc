e496e7e5f7766663f7c78dbb07a929f671a9d182
austin-hoover <ahoover7@tennessee.edu>
2021-01-20 18:36:25 -0500

    Add ability to track particles through nonlinear field outside uniform density, elliptical beam core
    The method is not tested yet.

--- src/spacecharge/EnvSolver.cc
+++ src/spacecharge/EnvSolver.cc
@@ -9,50 +9,76 @@ EnvSolver::EnvSolver(double perveance): CppPyWrapper(NULL)
 
 void EnvSolver::trackBunch(Bunch* bunch, double length)
 {
-    double xx, yy, xy, _cos, _sin, cos2, sin2, sincos, factor;
     // Compute ellipse size and orientation
     a = bunch->x(0); e = bunch->y(0);
     b = bunch->x(1); f = bunch->y(1);
-    xx = a*a + b*b; // 4 * <x^2>
-    yy = e*e + f*f; // 4 * <y^2>
-    xy = a*e + b*f; // 4 * <xy>
+    double xx = a*a + b*b; // 4 * <x^2>
+    double yy = e*e + f*f; // 4 * <y^2>
+    double xy = a*e + b*f; // 4 * <xy>
     phi = -0.5 * atan2(2*xy, xx - yy);
-    _cos = cos(phi);
-    _sin = sin(phi);
-    cos2 = _cos * _cos;
-    sin2 = _sin * _sin;
-    sincos = _sin * _cos;
-    cx = sqrt(abs(xx*cos2 + yy*sin2 - 2*xy*sincos));
-    cy = sqrt(abs(xx*sin2 + yy*cos2 + 2*xy*sincos));
-    factor = 2 * Q / (cx + cy);
-    
+    double _cos = cos(phi);
+    double _sin = sin(phi);
+    double cos2 = _cos * _cos;
+    double sin2 = _sin * _sin;
+    double sincos = _sin * _cos;
+    double cx = sqrt(abs(xx*cos2 + yy*sin2 - 2*xy*sincos));
+    double cy = sqrt(abs(xx*sin2 + yy*cos2 + 2*xy*sincos));
+    double factor = length * (2 * Q / (cx + cy));
+        
     // Track envelope
     if (cx > 0) {
-        bunch->xp(0) += (factor * (a*cos2 - e*sincos)/cx) * length;
-        bunch->xp(1) += (factor * (b*cos2 - f*sincos)/cx) * length;
-        bunch->yp(0) += (factor * (e*sin2 - a*sincos)/cx) * length;
-        bunch->yp(1) += (factor * (f*sin2 - b*sincos)/cx) * length;
+        bunch->xp(0) += factor * (a*cos2 - e*sincos)/cx;
+        bunch->xp(1) += factor * (b*cos2 - f*sincos)/cx;
+        bunch->yp(0) += factor * (e*sin2 - a*sincos)/cx;
+        bunch->yp(1) += factor * (f*sin2 - b*sincos)/cx;
     }
     if (cy > 0) {
-        bunch->xp(0) += (factor * (a*sin2 + e*sincos)/cy) * length;
-        bunch->xp(1) += (factor * (b*sin2 + f*sincos)/cy) * length;
-        bunch->yp(0) += (factor * (e*cos2 + a*sincos)/cy) * length;
-        bunch->yp(1) += (factor * (f*cos2 + b*sincos)/cy) * length;
+        bunch->xp(0) += factor * (a*sin2 + e*sincos)/cy;
+        bunch->xp(1) += factor * (b*sin2 + f*sincos)/cy;
+        bunch->yp(0) += factor * (e*cos2 + a*sincos)/cy;
+        bunch->yp(1) += factor * (f*cos2 + b*sincos)/cy;
     }
     
     // Track bunch particles
-    //   To do: add Jeff's code to handle particles outside ellipse
-    double x, y;
+    //     To do:
+    //        * Double check formula for electric field outside envelope
+    //        * Add conducting boundary (Jeff's code)
+    double cx2 = cx * cx;
+    double cy2 = cy * cy;
+    double x, y, xn, yn, x2, y2, xn2, yn2;
+    double t1, B, C, D;
+    double delta_xpn, delta_ypn;
+    bool in_ellipse;
+    
     for (int i = 2; i < bunch->getSize(); i++) {
-        x = bunch->x(i); 
+        delta_xpn = 0.0;
+        delta_ypn = 0.0;
+        x = bunch->x(i);
         y = bunch->y(i);
-        if (cx > 0) {
-            bunch->xp(i) += (factor * (cos2*x - sincos*y)/cx) * length;
-            bunch->yp(i) += (factor * (sin2*y - sincos*x)/cx) * length;
+        xn = x * _cos - y * _sin;
+        yn = x * _sin + y * _cos;
+        x2 = x * x;
+        y2 = y * y;
+        xn2 = xn * xn;
+        yn2 = yn * yn;
+        in_ellipse = xn2/cx2 + yn2/cy2 <= 1.0;
+        if (in_ellipse) {
+            if (cx > 0.0) {
+                delta_xpn = factor * xn / cx;
+            }
+            if (cy > 0.0) {
+                delta_ypn = factor * yn / cy;
+            }
         }
-        if (cy > 0) {
-            bunch->xp(i) += (factor * (sin2*x + sincos*y)/cy) * length;
-            bunch->yp(i) += (factor * (cos2*y + sincos*x)/cy) * length;
+        else {
+            B = xn2 + yn2 - cx2 - cy2;
+            C = xn2*cy2 + yn2*cx2 - cx2*cy2;
+            t1 = pow(B*B/4 + C, 0.5) + B/2;
+            D = pow((cx2 + t1) * (cy2 + t1), 0.5);
+            delta_xpn = 2 * Q * xn / ((cx2 + t1) + D);
+            delta_ypn = 2 * Q * yn / ((cy2 + t1) + D);
         }
+        bunch->xp(i) += +delta_xpn * _cos + delta_ypn * _sin;
+        bunch->yp(i) += -delta_xpn * _sin + delta_ypn * _cos;
     }
 }
