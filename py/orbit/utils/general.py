0bca92ce7eb62f3bcffab81a616a4f9bab8a834a
austin-hoover <ahoover7@tennessee.edu>
2021-08-07 14:03:48 -0400

    Add delete_files_not_folders method

--- py/orbit/utils/general.py
+++ py/orbit/utils/general.py
@@ -1,4 +1,12 @@
+import os
+import sys
 import numpy as np
+
+
+def delete_files_not_folders(path):
+    for root, folders, files in os.walk(path):
+        for file in files:
+            os.remove(os.path.join(root, file))
 
 
 def tprint(string, indent=4):
@@ -40,32 +48,3 @@ def rotation_matrix_4D(angle):
     """4x4 matrix to rotate [x, x', y, y'] clockwise in the x-y plane."""
     c, s = np.cos(angle), np.sin(angle)
     return np.array([[c, 0, s, 0], [0, c, 0, s], [-s, 0, c, 0], [0, -s, 0, c]])
-
-
-# The following three functions are from Tony Yu's blog post: https://tonysyu.github.io/ragged-arrays.html#.YKVwQy9h3OR
-
-def stack_ragged(array_list, axis=0):
-    """Stacks list of arrays along first axis.
-    
-    Example: (25, 4) + (75, 4) -> (100, 4). It also returns the indices at
-    which to split the stacked array to regain the original list of arrays.
-    """
-    lengths = [np.shape(array)[axis] for array in array_list]
-    idx = np.cumsum(lengths[:-1])
-    stacked = np.concatenate(array_list, axis=axis)
-    return stacked, idx
-    
-
-def save_stacked_array(filename, array_list, axis=0):
-    """Save list of ragged arrays as single stacked array. The index from
-    `stack_ragged` is also saved."""
-    stacked, idx = stack_ragged(array_list, axis=axis)
-    np.savez(filename, stacked_array=stacked, stacked_index=idx)
-    
-    
-def load_stacked_arrays(filename, axis=0):
-    """"Load stacked ragged array from .npz file as list of arrays."""
-    npz_file = np.load(filename)
-    idx = npz_file['stacked_index']
-    stacked = npz_file['stacked_array']
-    return np.split(stacked, idx, axis=axis)
