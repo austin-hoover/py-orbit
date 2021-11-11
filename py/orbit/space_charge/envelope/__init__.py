3f69a4d0465504051e718075057c47a0b762c89d
austin-hoover <ahoover7@tennessee.edu>
2021-11-11 16:51:25 -0500

    Change class names

--- py/orbit/space_charge/envelope/__init__.py
+++ py/orbit/space_charge/envelope/__init__.py
@@ -1,10 +1,8 @@
-from orbit.space_charge.envelope.EnvSolverNode import DanilovEnvSolverNode
-from orbit.space_charge.envelope.EnvSolverLatticeModifications import set_env_solver_nodes
+from orbit.space_charge.envelope.DanilovEnvSolverNode import DanilovEnvSolverNode
+from orbit.space_charge.envelope.DanilovEnvSolverLatticeModifications import set_env_solver_nodes
 from orbit.space_charge.envelope.EnvSolverLatticeModifications import set_perveance
-from orbit.envelope.danilov import DanilovEnvelope
 
 __all__ = []
 __all__.append("DanilovEnvSolverNode")
 __all__.append("set_env_solver_nodes")
 __all__.append("set_perveance")
-__all__.append("DanilovEnvelope")
