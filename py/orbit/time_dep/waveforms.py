ba5c7e34979542301b204821c7ef47594b13c861
austin-hoover <ahoover7@tennessee.edu>
2021-11-12 16:23:44 -0500

    Create inverse square root waveform

--- py/orbit/time_dep/waveforms.py
+++ py/orbit/time_dep/waveforms.py
@@ -47,3 +47,22 @@ class SquareRootWaveform:
             dt = math.sqrt((t - self.t1) / (self.t2 - self.t1))
             amp = ((self.amp1 - self.amp2) * (1.0 - dt) + self.amp2)
         return amp
+    
+
+class InvSquareRootWaveform:
+    
+    def __init__(self, syncpart, t1, t2, amp1, amp2):
+        self.name = 'inverse square root waveform'
+        self.syncpart = syncpart
+        self.t1, self.t2, self.amp1, self.amp2 = t1, t2, amp1, amp2
+
+    def getAmplitude(self):
+        t = self.syncpart.time()
+        if t < self.t1:
+            amp = self.amp1
+        elif t > self.t2:
+            amp = self.amp2
+        else:
+            dt = math.sqrt((self.t2 - (t - self.t1)) / (self.t2 - self.t1))    
+            amp = ((self.amp1 - self.amp2) * (1.0 - dt) + self.amp2)
+        return amp
